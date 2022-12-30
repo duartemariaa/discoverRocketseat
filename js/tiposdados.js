08b9ecb36dc87e08145320f71b3589a984bf5069
mariadrt <107371314+duartemariaa@users.noreply.github.com>
2022-12-30 16:40:09 -0300

    tipos de dados
    Inicializando os estudos de JavaScript

---
+++ js/tiposdados.js
@@ -0,0 +1,58 @@
+//strings
+
+console.log('Mary')
+
+console.log("Mary")
+
+console.log('"Mary"')
+
+console.log("'Mary'")
+
+console.log(`
+
+Mary
+
+`)
+
+console.log(`Mary ${1+1}`)
+
+//numbers
+
+console.log(33)
+console.log(-33)
+console.log(12.5)
+console.log(3.3 + 2)
+
+console.log(12.5 / "abcd")
+
+console.log(12.5 ===  Infinity)
+
+console.log(Infinity)
+
+//boolean
+
+console.log(true)
+console.log(false)
+
+//Undefined vs null
+
+console.log(null === undefined)
+
+//object
+
+console.log({
+    name:"mary",
+    idade:22,
+    andar: function(){
+        console.log('andar')
+    }
+})
+
+//array
+
+console.log([
+    "leite", 
+    "ovos",
+    2,
+    3
+])
