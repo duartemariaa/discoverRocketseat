b0728ebfa1653c0dd00a01d30f28b2e5e15c8c93
mariadrt <107371314+duartemariaa@users.noreply.github.com>
2023-01-30 11:45:47 -0300

    ForEach

--- NodeJS-EJS/server.js
+++ NodeJS-EJS/server.js
@@ -5,7 +5,39 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res){
-    res.render("pages/index")
+    const items = [
+        {
+            title: "D",
+            message: "esenvolver aplicações/serviços de forma fácil"
+        },
+        {
+            title: "E",
+            message: "JS usa JavaScript para renderizar HTML"
+        },
+        {
+            title: "M",
+            message: "uito fácil de usar"
+        },
+        {
+            title: "A",
+            message:"morzinho"
+        },
+        {
+            title: "I",
+            message: "nstal ejs"
+        },
+        {
+            title: "S",
+            message: "intaxe simples"
+        }
+    ];
+
+    const subtitle = "Uma linguagem de modelgem para criação de página HTML utilizando JavaScript"
+
+    res.render("pages/index", {
+        qualitys: items,
+        subtitle: subtitle,
+    });
 })
 
 app.get("/sobre", function (req, res){
