9519b0e0305f5677ffe99157bd9f456dc384f524
Maria Duarte <mariaduarte061@hotmail.com>
2023-02-14 10:43:10 -0300

    add requests in parameters
    Body Params, Route Params, query Params

--- APIs/index.js
+++ APIs/index.js
@@ -28,6 +28,41 @@ app.route('/').put((req,res) => {
 
 
 
-app.route('/:identificador').delete((req,res) => {
+/*app.route('/:identificador').delete((req,res) => {
     res.send(req.params.identificador)
-})
+})*/
+
+
+
+//Parâmetros nas requisições
+
+//body params
+//app.route('/').get((req, res) => res.send("ola"))
+
+/*app.use(express.json())
+app.route('/').post((req,res) => {
+    res.send(req.body) //res.send(req.body.nome)
+})*/
+
+/*app.use(express.json())
+app.route('/').post((req,res) => {
+    const {nome, cidade} = req.body
+    res.send(`meu nome é ${nome} e minha cidade é ${cidade}`)
+})*/
+
+
+
+//route params
+/*app.route('/').get((req, res) => res.send("hi"))
+
+app.route('/:variavel').get((req, res) => res.send(req.params.variavel))
+
+app.route('/identidade/:nome').get((req, res) => res.send(req.params.nome))*/
+
+
+
+//query params
+//app.route('/').get((req, res) => res.send(req.query))
+app.route('/').get((req, res) => res.send(req.query.nome))
+
+app.route('/about/user').get((req, res) => res.send(req.query))
