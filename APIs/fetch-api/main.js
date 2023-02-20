9e57853a1fc47c8297ca6de7cea69a4a43f43c85
Maria Duarte <mariaduarte061@hotmail.com>
2023-02-20 11:02:55 -0300

    update

--- APIs/fetch-api/main.js
+++ APIs/fetch-api/main.js
@@ -20,7 +20,7 @@ function getUser(){
 }
 
 
-//POST
+//POST //error
 function addUser(newUser){
     fetch(url, {
         method: "POST",
@@ -40,7 +40,48 @@ const newUser = {
     city: "Rio do Sul"
 }
 
-addUser(newUser)
+//addUser(newUser)
 
 getUsers()
 getUser()
+
+
+
+//PUT //error
+function updateUser(updatedUser, id) {
+    fetch(`${url}/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(updateUser),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+        .then(response => response.json())
+        .then(data => alertApi.textContent = data)
+        .catch(error => console.error(error))
+}
+
+const updatedUser = {
+    name: "Marcelo Clovis",
+    avatar: "htttp://lorempixel.com/400/200",
+    city: "Recife"
+}
+
+updateUser(updatedUser, 1)
+
+
+
+//DELETE
+function deleteUser(id) {
+    fetch(`${url}/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+        .then(response => response.json())
+        .then(data => alertApi.textContent = data)
+        .catch(error => console.error(error))
+}
+
+//deleteUser(1)
